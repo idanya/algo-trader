11e1393e654c28b09fa8b946d4e1b8a39ff18ebf
Idan Yael <idanya@wix.com>
2021-10-03 08:36:12 +0300

    mongo dups handling

--- src/storage/mongodb_storage.py
+++ src/storage/mongodb_storage.py
@@ -17,10 +17,24 @@ class MongoDBStorage(StorageProvider):
         super().__init__()
         self.client = pymongo.MongoClient(f'mongodb://{host}:{port}/')
         self.db = self.client[database]
+        self.candles_collection = self.db[CANDLES_COLLECTION]
+        self.candles_collection.create_index([("symbol", pymongo.ASCENDING),
+                                              ("timespan", pymongo.ASCENDING),
+                                              ("timestamp", pymongo.ASCENDING)],
+                                             unique=True, background=True)
 
     def save(self, candle: Candle):
-        collection = self.db[CANDLES_COLLECTION]
-        collection.insert_one(self._serialize_candle(candle))
+        self.candles_collection.replace_one(self._serialize_candle_key(candle), self._serialize_candle(candle),
+                                            upsert=True)
+        # self.candles_collection.insert_one(self._serialize_candle(candle))
+
+    def _serialize_candle_key(self, candle: Candle) -> Dict:
+        data = self._serialize_candle(candle)
+        return {
+            'symbol': data['symbol'],
+            'timespan': data['timespan'],
+            'timestamp': data['timestamp'],
+        }
 
     def _serialize_candle(self, candle: Candle) -> Dict:
         data = candle.serialize()
@@ -34,15 +48,13 @@ class MongoDBStorage(StorageProvider):
     def get_candles(self, symbol: str, time_span: TimeSpan,
                     from_timestamp: datetime, to_timestamp: datetime) -> List[
         Candle]:
-        collection = self.db[CANDLES_COLLECTION]
-
         query = {
             'symbol': symbol,
             'timespan': time_span.name,
             'timestamp': {"$gte": from_timestamp, "$lte": to_timestamp}
         }
 
-        return [self._deserialize_candle(candle) for candle in collection.find(query).sort("timestamp")]
+        return [self._deserialize_candle(candle) for candle in self.candles_collection.find(query).sort("timestamp")]
 
     def __drop_collections__(self):
         self.db.drop_collection(CANDLES_COLLECTION)
