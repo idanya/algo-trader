f79c203eef0dc8fb95b421d6960e6ed53fc20604
Idan Yael <iyael@fireblocks.com>
2022-11-13 09:00:31 +0200

    fix flacky test

--- tests/unit/test_asset_correlation.py
+++ tests/unit/test_asset_correlation.py
@@ -22,16 +22,11 @@ class TestAssetCorrelationProcessor(TestCase):
     def setUp(self) -> None:
         super().setUp()
         x = [generate_candle_with_price_and_symbol('X', TimeSpan.Day, datetime.now() - timedelta(days=c),
-                                                   c + random.randint(1, 10)) for c in
-             range(1, 49)]
+                                                   c + random.randint(1, 10)) for c in range(1, 49)]
         y = [generate_candle_with_price_and_symbol('Y', TimeSpan.Day, datetime.now() - timedelta(days=c),
-                                                   c + random.randint(1, 10)) for c
-             in
-             range(1, 49)]
+                                                   c + random.randint(1, 10)) for c in range(1, 49)]
         z = [generate_candle_with_price_and_symbol('Z', TimeSpan.Day, datetime.now() - timedelta(days=c),
-                                                   c + random.randint(1, 10)) for c
-             in
-             range(1, 49)]
+                                                   c + random.randint(1, 10)) for c in range(1, 49)]
 
         merged: List[Candle] = []
         for i in range(len(x)):
@@ -54,14 +49,15 @@ class TestAssetCorrelationProcessor(TestCase):
                     CORRELATIONS_ATTACHMENT_KEY)
                 if candle.symbol == 'X':
                     self.assertFalse(asset_correlation.has('X'))
-                    self.assertTrue(asset_correlation.get('Y') > 0)
-                    self.assertTrue(asset_correlation.get('Z') > 0)
+                    self.assertTrue(asset_correlation.has('Y'))
+                    self.assertTrue(asset_correlation.has('Z'))
                 else:
-                    self.assertTrue(asset_correlation.get('X') > 0)
+                    self.assertTrue(asset_correlation.has('X'))
 
         validator = ValidationProcessor(_check)
         cache_processor = CandleCache(validator)
-        correlations_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../configs/correlations.json')
+        correlations_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                              '../configs/correlations.json')
         asset_correlation = AssetCorrelationProcessor(correlations_file_path, cache_processor)
         timespan_change_processor = TimeSpanChangeProcessor(TimeSpan.Day, asset_correlation)
         technicals = TechnicalsProcessor(timespan_change_processor)
