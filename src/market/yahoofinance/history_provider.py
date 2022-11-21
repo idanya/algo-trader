01d03bf773172fd073c5d1762fe743de98c22bff
Idan Yael <iyael@fireblocks.com>
2022-11-21 23:35:54 +0200

    get_symbol_history

--- src/market/yahoofinance/history_provider.py
+++ src/market/yahoofinance/history_provider.py
@@ -1,11 +1,37 @@
+from datetime import datetime
+from typing import List
+
 import yfinance as yf
 
+from entities.candle import Candle
+from entities.timespan import TimeSpan
+
+
 class YahooFinanceHistoryProvider:
-    def __init__(self):
-        pass
-
-    def get_symbol_history(self, symbol: str):
+    def get_symbol_history(self, symbol: str, period: TimeSpan, interval: TimeSpan,
+                           start_time: datetime, end_time: datetime, auto_adjust: bool = True,
+                           include_after_hours: bool = False) -> List[Candle]:
         ticker = yf.Ticker(symbol)
+        data = ticker.history(self._translate_timespan(period),
+                              self._translate_timespan(interval),
+                              start_time, end_time, include_after_hours,
+                              False, auto_adjust)
 
+        candles: List[Candle] = []
+        for index, row in data.iterrows():
+            candle = Candle(symbol, period, index.to_pydatetime(),
+                            row['Open'], row['Close'], row['High'], row['Low'], row['Volume'])
+            candles.append(candle)
 
+        return candles
 
+    @staticmethod
+    def _translate_timespan(span: TimeSpan) -> str:
+        if span == TimeSpan.Day:
+            return '1d'
+        elif span == TimeSpan.Hour:
+            return '1h'
+        elif span == TimeSpan.Minute:
+            return '1m'
+        else:
+            raise ValueError('minimum timespan for yahoo finance is 1m')
